08c1ba33093441f7dcf05f11586991f2bd7f3c58
pravinyadavhub <work.coderpravin@gmail.com>
2018-03-31 18:07:32 +0530

    routers added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
+// Import modules
+
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BlogComponent } from './components/blog/blog.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path : 'login' , component : LoginComponent},
+  { path : 'register' , component : RegisterComponent},
+  { path : 'dashboard', component : DashboardComponent},
+  { path : 'profile', component : ProfileComponent},
+  { path : 'blog', component : BlogComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ProfileComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
